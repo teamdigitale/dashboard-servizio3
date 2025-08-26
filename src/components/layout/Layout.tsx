3c35ee1ef719a8db913b923b6f54d3f24c0f7b7e
Lorezz <point.point@gmail.com>
2025-08-26 14:58:28 +0200

    fixed tailwind

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -18,11 +18,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       className='bg-base-100 flex flex-col h-screen w-screen overflow-hidden max-h-screen max-w-screen'
       data-theme={currentTheme}
     >
-      <div className='shadow-md h-16 w-full flex-shrink-0'>
+      <div className='shadow-md h-16 w-full shrink-0'>
         <Navbar currentTheme={settings?.preferredTheme || "light"} />
       </div>
       <div className='grow-1 overflow-y-auto'>{children}</div>
-      <footer className='h-16 w-full flex-shrink-0 footer footer-center p-4 bg-base-200 text-content border-t border-content'>
+      <footer className='h-16 w-full shrink-0 footer footer-center p-4 bg-base-200 text-content border-t border-content'>
         <div>
           <p>{new Date().getFullYear()} - Servizio3.</p>
         </div>
