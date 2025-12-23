2b9acc7863b23885cc238ed843db05361f65a783
Lorezz <point.point@gmail.com>
2025-12-23 15:58:35 +0100

    sample with texts

--- src/routes/Fse.tsx
+++ src/routes/Fse.tsx
@@ -27,32 +27,132 @@ const groups: chartType[] = [
 	{
 		id: "primo",
 		name: "Indicatore 1",
-		description: "Descrizione dell'indicatore 1",
-		content: "contents",
+		description: "N° Documenti indicizzati / N° prestazioni erogate",
+		content: `
+		<p>
+		<h3>Media Nazionale</h3>
+		<p>Ultima rilevazione: settembre 2025</p>
+		<br/>
+		<b>Numeratore</b>
+		Somma del numero di documenti di:
+		<ul>
+		<li>Referto di Medicina di Laboratorio</li>
+		<li>Referto di Radiologia</li>
+		<li>Verbale di Pronto Soccorso</li>
+		<li>Lettera di Dimissione</li>
+		<li>Referto di Specialistica Ambulatoriale</li>
+		<li>Referto di Anatomia Patologica</li>
+		</ul>
+		<b>Denominatore</b>
+		Somma di:
+		<ul>
+		</p>
+		<p>
+		<li>Prestazioni di Laboratorio</li>
+		<li>Prestazioni di Radiologia</li>
+		<li>Accessi al Pronto Soccorso</li>
+		<li>Numero di ricoveri</li>
+		<li>Prestazioni di Ambulatoriale</li>
+		<li>Prestazioni di Anatomia Patologica</li>
+		</ul>
+		</p>
+		`,
 		pie: idPies[0],
 		map: idMaps[0],
 	},
 	{
 		id: "secondo",
 		name: "Indicatore 2",
-		description: "Descrizione dell'indicatore 2",
-		content: "contents",
+		description: "N° MMG/PLS che alimentano il FSE /N° MMG/PLS totali",
+		content: `
+		<p>
+		<h3>Media Nazionale</h3>
+		<p>Ultima rilevazione: settembre 2025</p>
+		<br/>
+		<b>Numeratore</b>
+		<ul>
+		<li>Numero di operatori con ruolo MMG-PLS che hanno invitato o sono firmatari di documenti in formato pdf con CDA2 iniettato e firmato, oppure che hanno correttamente conferito ricette dematerializzate al SAC</li>
+		</ul>
+		<b>Denominatore</b>
+		<ul>
+		</p>
+		<p>
+		<li>Numeri di  MMG-PLS totali</li>
+		</ul>
+		</p>
+		`,
 		pie: idPies[1],
 		map: idMaps[1],
 	},
 	{
 		id: "terzo",
 		name: "Indicatore 3",
-		description: "Descrizione dell'indicatore 3",
-		content: "contents",
+		description: "N. Documenti CDA2 / N. Documenti indicizzati",
+		content: `
+		<p>
+		<h3>Media Nazionale</h3>
+		<p>Ultima rilevazione: settembre 2025</p>
+		<br/>
+		<b>Numeratore</b>
+		Somma del numero di documenti in formato pdf con CDA2 iniettato di tipo:
+		<ul>
+		<li>Referto di Medicina di Laboratorio</li>
+		<li>Referto di Radiologia</li>
+		<li>Verbale di Pronto Soccorso</li>
+		<li>Lettera di Dimissione</li>
+		<li>Referto di Specialistica Ambulatoriale</li>
+		<li>Referto di Anatomia Patologica</li>
+		</ul>
+		<b>Denominatore</b>
+		Somma di tutti i documenti firmati e non di tipo	:
+		<ul>
+		</p>
+		<p>
+		<li>Prestazioni di Laboratorio</li>
+		<li>Prestazioni di Radiologia</li>
+		<li>Accessi al Pronto Soccorso</li>
+		<li>Numero di ricoveri</li>
+		<li>Prestazioni di Ambulatoriale</li>
+		<li>Prestazioni di Anatomia Patologica</li>
+		</ul>
+		</p>
+		`,
 		pie: idPies[2],
 		map: idMaps[2],
 	},
 	{
 		id: "quarto",
 		name: "Indicatore 4",
-		description: "Descrizione dell'indicatore 4",
-		content: "contents",
+		description: "N. Documenti firmati in PAdES / N. Documenti indicizzati",
+		content: `
+		<p>
+		<h3>Media Nazionale</h3>
+		<p>Ultima rilevazione: settembre 2025</p>
+		<br/>
+		<b>Numeratore</b>
+		Somma del numero di documenti in formato pdf con CDA2 iniettato e firmato di tipo:
+		<ul>
+		<li>Referto di Medicina di Laboratorio</li>
+		<li>Referto di Radiologia</li>
+		<li>Verbale di Pronto Soccorso</li>
+		<li>Lettera di Dimissione</li>
+		<li>Referto di Specialistica Ambulatoriale</li>
+		<li>Referto di Anatomia Patologica</li>
+		</ul>
+		<b>Denominatore</b>
+		Somma di tutti i documenti firmati e non di tipo	:
+		<ul>
+		</p>
+		<p>
+		<li>Prestazioni di Laboratorio</li>
+		<li>Prestazioni di Radiologia</li>
+		<li>Accessi al Pronto Soccorso</li>
+		<li>Numero di ricoveri</li>
+		<li>Prestazioni di Ambulatoriale</li>
+		<li>Prestazioni di Anatomia Patologica</li>
+		</ul>
+		</p>
+		`,
 		pie: idPies[3],
 		map: idMaps[3],
 	},
@@ -88,21 +188,23 @@ function App() {
 					<p className="text-gray-600 mb-4">
 						{(choosen as chartType).description}
 					</p>
-					<div className="grid grid-cols-4 gap-4">
-						<div className="flex items-center justify-center w-full min-h-94 bg-base-300 text-content rounded-md  shadow-md col-span-3">
+					<div className="grid grid-cols-6 gap-4">
+						<div className="flex items-center justify-center w-full min-h-94 bg-base-300 text-content rounded-md  shadow-md col-span-4">
 							<div className="shrink-0 w-1/2">
 								<DashChart id={(choosen as chartType).pie} />
 							</div>
 							<div className="shrink-0 w-1/2">
-								<div
-									className="text-md p-4"
-									dangerouslySetInnerHTML={{
-										__html: (choosen as chartType).content,
-									}}
-								/>
+								<div className="text-content p-4 ">
+									<div
+										className="prose text-xs"
+										dangerouslySetInnerHTML={{
+											__html: (choosen as chartType).content,
+										}}
+									/>
+								</div>
 							</div>
 						</div>
-						<div className="flex ids-center justify-center w-full min-h-94 bg-base-300 text-content rounded-md  shadow-md">
+						<div className="flex ids-center justify-center w-full min-h-94 bg-base-300 text-content rounded-md  shadow-md col-span-2">
 							<DashChart id={(choosen as chartType).map} />
 						</div>
 					</div>
