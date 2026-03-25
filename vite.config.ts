d27df7710100c5bdaa82c74953e8cebaa0936b9d
Lorezz <point.point@gmail.com>
2026-03-25 16:23:46 +0100

    gs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const serverUrl = env.VITE_SERVER_URL ?? "http://localhost:3000";
+
   return {
     plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": serverUrl,
+      },
+    },
   };
 });
